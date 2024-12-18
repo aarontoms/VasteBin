9dc5ddb9ca93ea2419bd3e35297f96921b7fb222
aarontoms <aarontoms29@gmail.com>
2024-12-18 18:01:37 +0530

    UI changes for valid/invalid user

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,15 +1,19 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+// import Cookies from 'js-cookie';
 
 function Create() {
     const { username } = useParams<{ username?: string }>();
     const [isValidUser, setIsValidUser] = useState(false);
     const [wasteid, setWasteid] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
+    const [vaste, setVaste] = useState("");
 
     useEffect(() => {
         const fun = async () => {
             const localid = localStorage.getItem("userId");
+            // const sessionCookieValue = Cookies.get('vaste-session');
+            // console.log('Session Cookie Value:', sessionCookieValue);
 
             const response = await fetch(`http://127.0.0.1:5000/validate/${username}`, {
                 method: "POST",
@@ -48,16 +52,22 @@ function Create() {
                     placeholder="Enter Vaste ID"
                     className="bg-gray-800 p-2 text-gray-300 text-xl font-mono rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-gray-500 w-64"
                 />
-                <button
-                    className="ml-4 bg-gray-800 hover:bg-gray-900 text-white px-2 py-2 mx-10 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-green-400 text-xl font-semibold">
-                    Create Vaste
-                </button>
+                {isValidUser &&
+                    <button
+                        className="ml-4 bg-gray-800 hover:bg-gray-900 text-gray-300 px-2 py-2 mx-10 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-green-400 text-xl font-semibold">
+                        Create Vaste
+                    </button>
+                }
 
             </div>
             <div className="px-4">
                 <textarea
-                    value={isValidUser ? `${username}'s New Vaste` : errorMessage || ""}
-                    readOnly
+                    placeholder={!errorMessage && !vaste ? "Enter new Vaste here..." : ""}
+                    value={errorMessage ? errorMessage : vaste}
+                    onChange={(e) => {
+                        if (!errorMessage) setVaste(e.target.value);
+                    }}                    
+                    readOnly={!!errorMessage}
                     className={`w-full bg-zinc-700 font-mono p-4 rounded-lg shadow-md outline-none resize-none min-h-[80vh] overflow-y-auto
                         ${errorMessage ? "text-red-500" : "text-gray-300"}`}
                 />
