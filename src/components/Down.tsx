5bad227305dc9176ef884301c627b0489fed8d24
aarontoms <aarontoms29@gmail.com>
2024-12-21 20:49:02 +0530

    vaste ui changes

--- src/components/Down.tsx
+++ src/components/Down.tsx
@@ -3,7 +3,7 @@ import Waste from "./Waste";
 import { Link } from "react-router-dom";
 
 function Down({ search }: { search: string }) {
-    const [results, setResults] = useState<{ id: string; wastes: { id: string; value: string; created_at:string }[] } | null>(null);
+    const [results, setResults] = useState<{ id: string; wastes: { id: string; value: string; created_at: string }[] } | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
@@ -27,22 +27,22 @@ function Down({ search }: { search: string }) {
 
     return (
         <div className="m-10">
-          {error && <div className="text-red-500 text-center">{error}</div>}
-          {results && (
-            <div className="flex flex-col">
-              {results.wastes.map((waste) => (
-                <Link to={`${waste.id}`} key={waste.id} className="block">
-                  <Waste waste={waste.id} value={waste.value} created_at={waste.created_at}/>
-                </Link>
-              ))}
-              <Link to={`create`} className="block">
-                <Waste waste="Create new Waste" value="assmans" created_at=""/>
-              </Link>
-            </div>
-          )}
+            {error && <div className="text-red-500 text-center">{error}</div>}
+            {results && (
+                <div className="flex flex-col">
+                    {results.wastes.map((waste) => (
+                        <Link to={`${waste.id}`} key={waste.id} className="block">
+                            <Waste waste={waste.id} value={waste.value} created_at={waste.created_at} />
+                        </Link>
+                    ))}
+                    <Link to={`create`} className="block">
+                        <Waste waste="Create new Waste" value="assmans" created_at="" />
+                    </Link>
+                </div>
+            )}
         </div>
-      );
-      
+    );
+
 }
 
 export default Down;
