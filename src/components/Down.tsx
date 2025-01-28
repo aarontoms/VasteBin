c5879e00c122e276424c381f5d36fe861daa98fc
aarontoms <aarontoms29@gmail.com>
2025-01-28 21:34:31 +0530

    date ui change & link to repo

--- src/components/Down.tsx
+++ src/components/Down.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Waste from "./Waste";
 import { Link } from "react-router-dom";
 import url from "./url";
+import github from "../assets/github.png";
 
 function Down({ search }: { search: string }) {
     const [results, setResults] = useState<{ id: string; wastes: { id: string; value: string; created_at: string }[] } | null>(null);
@@ -27,21 +28,45 @@ function Down({ search }: { search: string }) {
     }, [search]);
 
     return (
-        <div className="m-10">
-            {error && <div className="text-red-500 text-center">{error}</div>}
-            {results && (
-                <div className="flex flex-col">
-                    {results.wastes.map((waste) => (
-                        <div className="flex justify-between items-center">
-                            <Link to={`${waste.id}`} key={waste.id} className="block">
-                                <Waste waste={waste.id} value={waste.value} created_at={waste.created_at} />
+        <div>
+            {!search ? (
+                <div className="flex flex-col justify-end">
+                    <div className="m-10 text-center justify-center">
+                        <a
+                            href="https://github.com/aarontoms/VasteBin"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-600 text-l no-underline hover:no-underline flex items-center justify-end gap-5"
+                        >
+                        <p>For documentation visit GitHub</p>
+                        <img src={github} alt="github" className="max-w-[30px] max-h-[30px] object-contain"/>
+                        </a>
+                    </div>
+                </div>
+            ) : (
+                <div className="m-10">
+                    {error && <div className="text-red-500 text-center">{error}</div>}
+                    {results && (
+                        <div className="flex flex-col">
+                            {results.wastes.map((waste) => {
+                                const gmtDate = new Date(waste.created_at);
+                                const options = { timeZone: "Asia/Kolkata", hour12: true };
+                                const istDate = gmtDate.toLocaleString("en-GB", options);
+
+                                return (
+                                    <div className="flex justify-between items-center" key={waste.id}>
+                                        <Link to={`${waste.id}`} className="block">
+                                            <Waste waste={waste.id} value={waste.value} created_at={istDate} />
+                                        </Link>
+                                    </div>
+                                );
+                            })}
+
+                            <Link to={`create`} className="block">
+                                <Waste waste="Create new Waste" value="assmans" created_at="" />
                             </Link>
                         </div>
-
-                    ))}
-                    <Link to={`create`} className="block">
-                        <Waste waste="Create new Waste" value="assmans" created_at="" />
-                    </Link>
+                    )}
                 </div>
             )}
         </div>
